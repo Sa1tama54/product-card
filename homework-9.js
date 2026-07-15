c0175e116574d038d4a1c284ad2ee026d7276790
Suleiman <ibn.alauddin123@gmail.com>
2026-07-15 22:03:33 +0300

    Add homework-9 level-2 tasks

--- homework-9.js
+++ homework-9.js
@@ -1,4 +1,6 @@
 // Задание 2. Отфильтровать массив так, чтобы получить числа начиная с 5
+import { comments } from "./comments.js";
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const filteredNumbers = numbers.filter((num) => num >= 5);
 
@@ -20,3 +22,40 @@ console.log(filteredNumbers);
 console.log(hasSmartphones);
 console.log(reverseArray(numbers));
 console.log(reverseArray(categories));
+
+// Задание 7. Комментарии, почта которых содержит ".com"
+const filteredComments = comments.filter((comment) => {
+  return comment.email.includes(".com");
+});
+
+console.log(filteredComments);
+
+// Задание 8. Изменить postId в зависимости от id пользователя
+const commentsWithPostId = comments.map((comment) => {
+  return {
+    ...comment,
+    postId: comment.id <= 5 ? 2 : 1,
+  };
+});
+
+console.log(commentsWithPostId);
+
+// Задание 9. Оставить в объектах только id и name
+const commentsWithIdAndName = comments.map((comment) => {
+  return {
+    id: comment.id,
+    name: comment.name,
+  };
+});
+
+console.log(commentsWithIdAndName);
+
+// Задание 10. Добавить свойство isInvalid
+const commentsWithValidity = comments.map((comment) => {
+  return {
+    ...comment,
+    isInvalid: comment.body.length > 180,
+  };
+});
+
+console.log(commentsWithValidity);
